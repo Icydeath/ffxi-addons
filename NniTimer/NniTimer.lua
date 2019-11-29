5961b081ed18ebdd3db2bd47bcf41196010003c5
Icydeath <Icydeath@users.noreply.github.com>
2019-11-29 11:58:51 -0500

    Updated nnitimer. Shows more info displayed.

--- NniTimer/NniTimer.lua
+++ NniTimer/NniTimer.lua
@@ -1,8 +1,16 @@
--- Addon is based off ZoneTimer
-
+--[[ Addon is based off ZoneTimer
+ 1.0.0.2
+	> Cleaned up text layout.
+	> White base text, turns green when floor is completed.
+	> Now shows + and - effects received.
+	
+ 1.0.0.1
+ 	> 18, 38, 58, 78, 98 changes the text to yellow to indicate a boss floor is close
+ 	> 19, 39, 59, 79, 99 changes the text to orange to indicate a boss floor is next	
+]]
 _addon.name = 'NniTimer'
 _addon.author = 'Icy'
-_addon.version = '1.0.0.0'
+_addon.version = '1.0.0.2'
 _addon.command = 'nnitimer'
 
 require('logger')
@@ -10,14 +18,20 @@ config = require('config')
 texts = require('texts')
  
 defaults = {}
+defaults.show_effects_received = true
 defaults.unload_zonetimer = false
+
 defaults.pos = {}
 defaults.pos.x = 600
 defaults.pos.y = 0
+
+defaults.bg = {}
+defaults.bg.alpha = 165
+
 defaults.text = {}
 defaults.text.font = 'Arial'
-defaults.text.red = 0
-defaults.text.blue = 0
+defaults.text.red = 255
+defaults.text.blue = 255
 defaults.text.green = 255
 defaults.text.size = 12
  
@@ -28,6 +42,8 @@ floorNum = 0
 objective = ""
 secObjective = ""
 status = ""
+pEffect = ""
+nEffect = ""
 
 windower.register_event('zone change', function(new_zone, old_zone)
 	start_time = os.time()
@@ -56,7 +72,7 @@ windower.register_event('prerender', function()
     end
 	
 	seconds = os.time() - start_time
-	times:text(os.date('!%H:%M:%S', seconds).." | Floor "..floorNum.." | "..objective.." "..secObjective.." "..status)
+	times:text(" "..os.date('!%H:%M:%S', seconds).." • Floor "..floorNum.." • "..pEffect.." | "..nEffect.." • "..objective.." • "..secObjective.."  "..status)
 	--times:text(display)
 end)
 
@@ -117,32 +133,91 @@ windower.register_event('incoming text', function(original, modified, original_m
 		floorNum = split_original[table.getn(split_original)]:split('.')[1]
 		status = ""
 		secObjective = ""
+		pEffect = ""
+		nEffect = ""
+		
+		local fnum = tonumber(floorNum)
+		if fnum then
+			if fnum == 18 or fnum == 38 or fnum == 58 or fnum == 78 or fnum == 98 then
+				times:color(255, 239, 0)
+			elseif fnum == 19 or fnum == 39 or fnum == 59 or fnum == 79 or fnum == 99 then
+				times:color(255, 165, 0)
+			else
+				times:color(255, 255, 255)
+			end
+		else
+			times:color(255, 255, 255)
+		end
 		
 	elseif(windower.wc_match(original, "*Objective:*")) then
 		--objective = original:split(':')[2]
 		if(windower.wc_match(original, "*Eliminate enemy leader.*")) then
-			objective = "Enemy leader."
+			objective = "® Enemy leader ®"
 		elseif(windower.wc_match(original, "*Eliminate all enemies.*")) then
-			objective = "All enemies."
+			objective = "∞ All enemies ∞"
 		elseif(windower.wc_match(original, "*Eliminate specified enemy.*")) then
-			objective = "Specified enemy."
+			objective = "(1) Specified enemy (1)"
 		elseif(windower.wc_match(original, "*Eliminate specified enemies.*")) then
-			objective = "Family."
+			objective = "♀ Family ♀"
 		elseif(windower.wc_match(original, "*Activate all lamps.*")) then
-			objective = "Lamps."
-		elseif(windower.wc_match(original, "*Free floor.*")) then
-			objective = "Free floor."
+			objective = "☼ Lamps ☼"
+		elseif(windower.wc_match(original, "*Rune of Transfer activated.*")) then
+			objective = "☺☺☺ Free floor ☺☺☺"
 		end
 		
 	elseif(windower.wc_match(original, "*Rune of Transfer activated.*")) then
-		status = "[COMPLETE]"
+		status = " √ COMPLETE √ "
+		local fnum = tonumber(floorNum)
+		if fnum then
+			if fnum ~= 18 and fnum ~= 38 and fnum ~= 58 and fnum ~= 78 and fnum ~= 98 
+			  and fnum ~= 19 and fnum ~= 39 and fnum ~= 59 and fnum ~= 79 and fnum ~= 99 then
+				times:color(0, 255, 0)
+			end
+		else
+			times:color(0, 255, 0)
+		end
 		
 	elseif (windower.wc_match(original, "*Avoid discovery by archaic gears!*")) then
-		secObjective = "Avoid discovery by gears."
+		secObjective = "GEARS » Avoid discovery‼"
 		
 	elseif (windower.wc_match(original, "*Do not destroy archaic gears!*")) then
-		secObjective = "Don't destroy gears."
+		secObjective = "GEARS » Don't destroy‼"
+
+	elseif (windower.wc_match(original, "*activated at the same time.*")) and (not windower.wc_match(objective, "*SAME TIME.*")) then
+		objective = objective .. " » SAME TIME."
+		
+	elseif (windower.wc_match(original, "*activated in a specific order...*")) and (not windower.wc_match(objective, "*ORDER.*")) then
+		objective = objective .. " » ORDER."
 	
+	elseif (windower.wc_match(original, "*code has been registered.*")) and (not windower.wc_match(objective, "*CODE.*")) then
+		objective = objective .. " » CODE."
+	
+	-- positive effects
+	elseif settings.show_effects_received and (windower.wc_match(original, "*Received *")) then
+		split_original = original:split(' ')
+		if pEffect ~= "" then
+			pEffect = pEffect..", +"..split_original[2]:split('.')[1]
+		else
+			pEffect = "+"..split_original[2]:split('.')[1]
+		end
+		
+	-- negative effects
+	elseif settings.show_effects_received and (windower.wc_match(original, "*Afflicted by *")) then
+		split_original = original:split('by')
+		if nEffect ~= "" then
+			nEffect = nEffect..", -"..split_original[2]:split('.')[1]
+		else
+			nEffect = "-"..split_original[2]:split('.')[1]
+		end
+	
+	elseif settings.show_effects_received and (windower.wc_match(original, "*is restricted.*")) then
+		split_original = original:split('is')
+		if nEffect ~= "" then
+			nEffect = nEffect..", -"..split_original[1]
+		else
+			nEffect = "-"..split_original[1]
+		end
+		
 	-- syncs up the time based off msgs received from the instance.
 	elseif (windower.wc_match(original, "*Time Remaining: 1 minute (Earth time).*")) then -- 1 min warning: 1740
 		start_time = os.time() - 1740
