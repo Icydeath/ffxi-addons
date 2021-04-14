7bee27cc106c42d971bbc016f6d34bcc9a22cd5e
Icydeath <brent.gabel12@gmail.com>
2021-04-14 10:47:23 -0400

    Updated locke, now uses in-game equipment sets. See notes in lua.
    Reason for the change is because certain gearswaps will ignore the disabled slots and still equip stuff. For that reason, I've decided to not rely gearswap for swapping gear.

--- Locke/Locke.lua
+++ Locke/Locke.lua
@@ -1,49 +1,15 @@
 _addon.name = 'Locke'
 _addon.author = 'Icy'
 _addon.commands = {'locke','thf','th'}
-_addon.version = '2020.10.27'
+_addon.version = '2021.4.14'
 
 -- This addon was built to test Rufuso's TH Theory: https://www.youtube.com/watch?v=r1pLv_aF7-s
--- The addon leverages Gearswap and you'll need to add sets to your gearswap lua file. 
---[[ IE:
-	sets.TreasureHunter8 = set_combine(sets.engaged, { -- set should be at +5 TH. (combined with traits its a total of +8 TH)
-		sub="Gandring", 				-- TH +3
-		head="Wh. Rarab Cap +1", 		-- TH +1
-		waist="Chaac Belt", 			-- TH +1
-	}) 
-
-	sets.TreasureHunter9 = set_combine(sets.engaged, { -- +6 TH (total of 9)
-		sub="Gandring", 				-- TH +3
-		hands="Plunderer's Armlets +1", -- TH +3
-	}) 
-
-	sets.TreasureHunter10 = set_combine(sets.engaged, { -- +7 TH (total of 10)
-		sub="Gandring", -- TH +3
-		hands="Plunderer's Armlets +1", -- TH +3
-		waist="Chaac Belt", -- TH +1
-	}) 
-
-	sets.TreasureHunter11 = set_combine(sets.engaged, { -- +8 TH (total of 11)
-		sub="Gandring", 				-- TH +3
-		head="Wh. Rarab Cap +1", 		-- TH +1
-		hands="Plunderer's Armlets +1", -- TH +3
-		waist="Chaac Belt", 			-- TH +1
-	}) 
-
-	sets.TreasureHunter12 = set_combine(sets.engaged, { -- +9 TH (total of 12)
-		sub="Gandring", 				-- TH +3
-		hands="Plunderer's Armlets +1", -- TH +3
-		feet="Skulk. Poulaines +1" 		-- TH +3
-	}) 
-
-	sets.TreasureHunter13 = set_combine(sets.engaged, { -- +10 TH (total of 13)
-		sub="Gandring", 				-- TH +3
-		hands="Plunderer's Armlets +1", -- TH +3
-		waist="Chaac Belt",				-- TH +1
-		feet="Skulk. Poulaines +1" 		-- TH +3
-	})
-]]
-
+-- Removed the usage of GS, wasn't consistent enough for what I'm trying to accomblish.
+--[[ 
+Locke now it utilizes the games native equipment sets:
+1) Create 6 equipment sets in game that has +5 TH, TH +6, TH +7, TH +8, TH +9, TH +10
+2) Adjust the equipset_map table below
+--]]
 
 require('logger')
 require('strings')
@@ -69,18 +35,21 @@ defaults.bg.green = 30
 defaults.bg.blue = 30
 
 settings = config.load(defaults)
-active = true
+active = false
 debug_mode = false
 thf_status = texts.new('Locke: OFF', settings)
 
-th_sets = T{
-	[8]="sets.TreasureHunter8", 
-	[9]="sets.TreasureHunter9",
-	[10]="sets.TreasureHunter10",
-	[11]="sets.TreasureHunter11",
-	[12]="sets.TreasureHunter12",
-	[13]="sets.TreasureHunter13"
+-- remember that thf has a base of +3 TH, so a equipment set that has +5 takes your total to +8 TH.
+equipset_map = T{
+	-- Change the equipset # to your ingame equipment set #
+	{ th=8, 	equipset=8 },
+	{ th=9, 	equipset=9 },
+	{ th=10, 	equipset=10 },
+	{ th=11, 	equipset=11 },
+	{ th=12, 	equipset=12 },
+	{ th=13, 	equipset=13 },
 }
+
 equip_th_set = nil
 tagged_mobs = T{}
 
@@ -89,16 +58,23 @@ windower.register_event('addon command', function(...)
 	if #commands ~= 0 then
 		if commands[1]:lower() == "on" then
 			active = true
-			thf_status:text('Locke: awaiting for proc...')
+			windower.send_command('lua u gearswap;wait .7;input /equipset '..equipset_map:with('th', 8).equipset)
+			thf_status:text('Locke: awaiting proc...')
 		elseif commands[1]:lower() == "off" then
 			active = false
+			windower.send_command('lua l gearswap')
 			thf_status:text('Locke: OFF')
 		elseif commands[1]:lower() == "debug" then
 			debug_mode = not debug_mode
 			log("Debug Mode: "..(debug_mode and "ON" or "OFF"))
 		else
 			active = not active
-			thf_status:text('Locke: %s':format(active and 'awaiting for proc...' or 'OFF'))
+			if active then 
+				windower.send_command('lua u gearswap;wait .7;input /equipset '..equipset_map:with('th', 8).equipset)
+			else
+				windower.send_command('lua l gearswap')
+			end
+			thf_status:text('Locke: %s':format(active and 'awaiting proc...' or 'OFF'))
 		end
 		
 	end
@@ -113,14 +89,19 @@ function handle_incoming_chunk(id, data)
 			if packet['Target 1 Action 1 Has Added Effect'] then
                 if packet['Target 1 Action 1 Added Effect Message'] == 603 then
 					local proc_num = packet['Target 1 Action 1 Added Effect Param']
-					if th_sets[proc_num] then
-						equip_th_set = th_sets[proc_num]
-					else
-						equip_th_set = th_sets[8]
+					if proc_num then
+						local set = equipset_map:with('th', proc_num)
+						if set then
+							equip_th_set = set.equipset
+						elseif tonumber(proc_num) and tonumber(proc_num) > 13 then
+							equip_th_set = nil
+						else
+							equip_th_set = equipset_map:with('th', 8).equipset
+						end
 					end
 					tagged_mobs[target.id] = proc_num
+					thf_status:text('Locke: '..target.name..' → '..proc_num..' → equipset '..equip_th_set)
 					equip(equip_th_set)
-					thf_status:text('Locke: '..target.name..' → '..proc_num..' → '..equip_th_set)
                 end
             end
 		end
@@ -153,14 +134,13 @@ function check_actor(packet)
     return false
 end
 
-function equip(set)
-	if set then
-		if debug_mode then log("Equipping set:", set) end
-		windower.send_command('gs enable all;gs c forceequip;gs equip '..set..';wait .2;gs disable all')
+function equip(setnum)
+	if setnum then
+		if debug_mode then log("Equipping set:", setnum) end
+		cmd = 'input /equipset '..tostring(setnum)
+		windower.send_command(cmd)
 	else
-		if debug_mode then log("Enabling gear slots") end
 		equip_th_set = nil
-		windower.send_command('gs enable all;gs c forceequip')
 	end
 end
 
@@ -168,19 +148,20 @@ function reset()
 	active = false
 	tagged_mobs:clear()
 	equip_th_set = nil
-	windower.send_command('gs enable all;gs c forceequip')
 	thf_status:text('Locke: OFF')
+	windower.send_command('lua l gearswap')
 end
 
 function loaded()
-	windower.send_command('lua u Thfknife;lua u thtracker')
+	windower.send_command('lua u Thfknife;lua u thtracker;')
 	check_job()
 end
 
 function unload()
+	windower.send_command('lua l gearswap')
 	player = windower.ffxi.get_player()
 	if player and player.main_job:lower() == 'thf' then
-		windower.send_command('lua l Thfknife;') --lua u thtracker
+		windower.send_command('lua l Thfknife;')
 	end
 end
 
@@ -188,6 +169,7 @@ function check_job()
     player = windower.ffxi.get_player()
     if player and player.main_job:lower() == 'thf' then
 		reset()
+		windower.send_command('lua u Thfknife;lua u thtracker;')
 		thf_status:show()
     else
         reset()
@@ -196,6 +178,8 @@ function check_job()
 end
 
 function target_changed(idx)
+	if not active then return end
+	
 	player = windower.ffxi.get_player()
 	local target_id = windower.ffxi.get_mob_by_index(idx)
 	target_id = target_id and target_id.id or nil
@@ -203,23 +187,26 @@ function target_changed(idx)
 	
 	if player.status == 1 then
 		if tagged_mobs[target_id] then
-			equip_th_set = th_sets[tagged_mobs[target_id]]
+			equip_th_set = equipset_map:with('th', tagged_mobs[target_id]).equipset
 		else
-			equip_th_set = th_sets[8]
+			equip_th_set = equipset_map:with('th', 8).equipset
 			tagged_mobs[target_id] = 8
 		end
+		equip(equip_th_set)
 	end
 end
 
 function status_changed(new, old)
+	if not active then return end
+	
 	player = windower.ffxi.get_player()
-	if active and new == 1 then
+	if new == 1 then
 		local target = windower.ffxi.get_mob_by_target('t')
 		if target and not tagged_mobs[target.id] then
-			equip_th_set = th_sets[8]
+			equip_th_set = equipset_map:with('th', 8).equipset
 			tagged_mobs[target.id] = 8
 		elseif target and tagged_mobs[target.id] then
-			equip_th_set = th_sets[tagged_mobs[target.id]] or th_sets[8]
+			equip_th_set = equipset_map:with('th', tagged_mobs[target_id]).equipset or equipset_map:with('th', 8).equipset
 		end
 		equip(equip_th_set)
 	elseif new == 0 then
